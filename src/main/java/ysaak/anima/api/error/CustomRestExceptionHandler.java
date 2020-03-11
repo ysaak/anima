644446ceaf83b716c7db26650c5b3081a6024219
droth <damien.roth@gmail.com>
2020-03-11 21:10:39 +0100

    Remove unused exception

--- src/main/java/ysaak/anima/api/error/CustomRestExceptionHandler.java
+++ src/main/java/ysaak/anima/api/error/CustomRestExceptionHandler.java
@@ -8,16 +8,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import ysaak.anima.exception.ArchException;
 
 @ControllerAdvice(annotations = RestController.class)
 public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler({ ArchException.class })
-    public ResponseEntity<Object> handleArchException(ArchException ex, WebRequest request) {
-        final ApiError apiError = new ApiError(ex.getCode(), ex.getMessage());
-        return new ResponseEntity<>(apiError, new HttpHeaders(), HttpStatus.I_AM_A_TEAPOT);
-    }
 
     @ExceptionHandler({ Exception.class })
     public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
