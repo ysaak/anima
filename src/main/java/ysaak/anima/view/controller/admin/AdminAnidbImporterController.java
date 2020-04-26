70a165835287d36d0523ff1203b1cfa7cb9ed4a2
droth <damien.roth@gmail.com>
2020-04-26 22:30:21 +0200

    Review admin Anidb importer error management

--- src/main/java/ysaak/anima/view/controller/admin/AdminAnidbImporterController.java
+++ src/main/java/ysaak/anima/view/controller/admin/AdminAnidbImporterController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ysaak.anima.exception.FunctionalException;
 import ysaak.anima.service.importer.anidb.AnidbImporterService;
 import ysaak.anima.service.technical.TranslationService;
 import ysaak.anima.view.controller.AbstractViewController;
@@ -16,32 +17,32 @@ import ysaak.anima.view.router.RoutingService;
 @RequestMapping("/admin/importer/anidb")
 @Transactional
 public class AdminAnidbImporterController extends AbstractViewController {
+    private static final String ROUTE_INDEX = "admin.importer.anidb.index";
+    private static final String ROUTE_TITLES_RELOAD = "admin.importer.anidb.reload-titles";
 
     private final AnidbImporterService anidbImporterService;
-    private final TranslationService translationService;
 
     public AdminAnidbImporterController(AnidbImporterService anidbImporterService, TranslationService translationService, RoutingService routingService) {
         super(translationService, routingService);
         this.anidbImporterService = anidbImporterService;
-        this.translationService = translationService;
     }
 
-    @GetMapping(path = "/", name = "admin.importer.anidb.index")
+    @GetMapping(path = "/", name = ROUTE_INDEX)
     public String indexAction(ModelMap model) {
         model.put("title_count", anidbImporterService.getAvailableTitleCount());
         return "admin/anidb/index";
     }
 
-    @PostMapping(path = "/reload-titles", name = "admin.importer.anidb.reload-titles")
+    @PostMapping(path = "/reload-titles", name = ROUTE_TITLES_RELOAD)
     public String reloadTitlesAction(final RedirectAttributes redirectAttributes) {
         try {
             anidbImporterService.refreshTitleList();
             this.addFlashInfoMessage(redirectAttributes, translationService.get("importer.anidb.title-list-reloaded.success"));
         }
-        catch (Exception e) {
+        catch (FunctionalException e) {
             this.addFlashErrorMessage(redirectAttributes, translationService.get("importer.anidb.title-list-reloaded.error"));
         }
 
-        return "redirect:/admin/importer/anidb/";
+        return redirect(ROUTE_INDEX);
     }
 }
